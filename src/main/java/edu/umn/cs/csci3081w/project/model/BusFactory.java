1e2672c2a883c2d16388d1129670a5e4ef7c9d59
Xinyue Yu <yu000291@umn.edu>
2020-11-21 16:27:16 -0600

    Task7

--- src/main/java/edu/umn/cs/csci3081w/project/model/BusFactory.java
+++ src/main/java/edu/umn/cs/csci3081w/project/model/BusFactory.java
@@ -2,6 +2,7 @@ package edu.umn.cs.csci3081w.project.model;
 import java.util.Random;
 
 public abstract class BusFactory {
+
   public abstract Bus makeRandomBus(String name, Route outbound, Route inbound, double speed);
 
   public abstract Bus makeStrategyBus(String name, Route outbound, Route inbound, double speed);
