ecd0613762ba9397d541fdad3b64ab5f41029405
Xinyue Yu <yu000291@umn.edu>
2020-11-21 19:38:14 -0600

    Task7 update

--- src/main/java/edu/umn/cs/csci3081w/project/model/BusFactory.java
+++ src/main/java/edu/umn/cs/csci3081w/project/model/BusFactory.java
@@ -1,6 +1,18 @@
 package edu.umn.cs.csci3081w.project.model;
-import java.util.Random;
+import java.time.LocalDateTime;
 
 public abstract class BusFactory {
+  public Bus busSelection(String name, Route outbound, Route inbound, double speed){
+    LocalDateTime dateOfNow=LocalDateTime.now();
+    int timeOfDate=dateOfNow.getDayOfMonth();
+    if(timeOfDate==1 || timeOfDate ==15){
+      Bus bus=makeBus(name,outbound,inbound,speed);
+      return bus;
+    }
+    else{
+      Bus bus=makeBus(name,outbound,inbound,speed);
+      return bus;
+    }
+  }
   public abstract Bus makeBus(String name, Route outbound, Route inbound, double speed);
 }
